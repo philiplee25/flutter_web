e733b742df4e00a9a5976ef415cba6c8959dd7a4
leepl <yplunap25@gmail.com>
2024-04-07 23:34:15 +0900

    feat: webview

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,10 +12,19 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('웹브라우저'),
+      ),
+      body: const WebView(
+        initialUrl: 'https://flutter.dev',
       ),
     );
   }
